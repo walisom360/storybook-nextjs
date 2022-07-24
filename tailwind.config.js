61ecf9fb6b0a269492e6dc352e77e3d8adefc6f3
walison matheus <walisom360@hotmail.com>
2022-07-24 19:57:28 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  // Ensure these match with .storybook/preview.js
+  theme: {
+    screens: {
+      xs: '375px',
+      sm: '600px',
+      md: '900px',
+      lg: '1200px',
+      xl: '1536px',
+    },
+  },
+  plugins: [],
+};
